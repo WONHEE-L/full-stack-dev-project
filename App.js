d8ce2385f207b6366db882cf31080c637db34c91
WONHEELEE <88381845+WONHEE-L@users.noreply.github.com>
2023-02-12 10:20:06 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h2>My Todos</h2>
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+      WonHee Lee
+        </a>
+        
+      </header>
+     
+    </div>
+  );
+}
+
+export default App;
+
+
+
